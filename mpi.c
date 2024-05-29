ff0f8cc670540d88fefe70d49fc87b2cc0d672f3
Julia Gonik <julia.gonik@snappler.com>
2024-05-28 23:17:47 -0300

    Funciona y a mimir

--- mpi.c
+++ mpi.c
@@ -5,9 +5,8 @@
 #include <time.h>
 
 // Definición de las variables globales
-double *A;
+double *A, *B;
 int N;
-int iguales = 1;
 
 // Función para calcular el tiempo
 double dwalltime()
@@ -128,6 +127,8 @@ int main(int argc, char *argv[])
 {
     int rank, size, local_N;
     double start_time, end_time;
+    int local_iguales = 1;
+    int global_iguales;
 
     // Inicializar MPI
     MPI_Init(&argc, &argv);
@@ -149,10 +150,8 @@ int main(int argc, char *argv[])
     local_N = N / size; // Calcular el tamaño de la porción local
 
     // Reservar memoria para los arreglos
-    if (rank == 0)
-    {
-        A = (double *)malloc(sizeof(double) * N);
-    }
+    A = (double *)malloc(sizeof(double) * N);
+
     // Reservar memoria para las porciones locales de los arreglos
     double *local_A = (double *)malloc(sizeof(double) * N);
     double *local_B = (double *)malloc(sizeof(double) * N);
@@ -160,23 +159,12 @@ int main(int argc, char *argv[])
     if (rank == 0)
     {
         // Inicializar el generador de números aleatorios y el arreglo A en el proceso principal
-        // srand(time(NULL));
-        // for (int i = 0; i < N / 2; i++)
-        // {
-        //     A[i] = (double)(rand() % 100); // Llenar la primera mitad del arreglo A con valores aleatorios
-        //     A[i + N / 2] = A[i];           // Copiar la primera mitad del arreglo A a la segunda mitad
-        // }
-
-        // }
-        A[0] = 1;
-        A[1] = 1;
-        A[2] = 0;
-        A[3] = 2;
-
-        A[4] = 2;
-        A[5] = 1;
-        A[6] = 1;
-        A[7] = 0;
+        srand(time(NULL));
+        for (int i = 0; i < N / 2; i++)
+        {
+            A[i] = (double)(rand() % 100); // Llenar la primera mitad del arreglo A con valores aleatorios
+            A[i + N / 2] = A[i];           // Copiar la primera mitad del arreglo A a la segunda mitad
+        }
 
         // Imprimir el arreglo original
         for (int i = 0; i < N; i++)
@@ -214,7 +202,7 @@ int main(int argc, char *argv[])
                 int end_index = start_index + section_size;     // 0 + 4 = 4 8
 
                 // Recibir los datos del proceso compañero
-                MPI_Recv(local_A + mid_index, section_size, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(local_A + mid_index, section_size / 2, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
                 printf("rank: %d recibió: ", rank);
                 for (int i = start_index; i < mid_index; i++)
@@ -228,7 +216,6 @@ int main(int argc, char *argv[])
                 }
                 printf("\n");
 
-                // Fusionar las dos mitades ordenadas de las secciones combinadas
                 if (remaining_processes > 2)
                 {
                     merge(local_A, local_B, start_index, mid_index, end_index);
@@ -239,32 +226,40 @@ int main(int argc, char *argv[])
         {
             int partner = rank - comparison_factor / 2;
             printf("ELSE rank: %d, partner: %d\n", rank, partner);
-
+            printf(" local_N * comparison_factor: %d\n", local_N * comparison_factor);
             // Enviar mis datos al proceso compañero
-            MPI_Send(local_A, local_N * comparison_factor, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD);
+            MPI_Send(local_A, (local_N * comparison_factor) / 2, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD);
             break;
         }
 
         remaining_processes /= 2;
     }
 
+    MPI_Scatter(local_A, local_N / 2, MPI_DOUBLE, local_A, local_N / 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(local_A + N / 2, local_N / 2, MPI_DOUBLE, local_A + N / 2, local_N / 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Verificar si las dos mitades del arreglo son iguales
+    for (int i = 0; i < local_N / 2; i++)
+    {
+        if (local_A[i] != local_A[i + N / 2])
+        {
+            local_iguales = 0;
+            break;
+        }
+    }
+
+    MPI_Reduce(&local_iguales, &global_iguales, 1, MPI_INT, MPI_LAND, 0, MPI_COMM_WORLD);
+
     if (rank == 0)
     {
         end_time = dwalltime(); // Detener el cronómetro
         printf("Tiempo en segundos usando MPI: %f\n", end_time - start_time);
+    }
 
-        // Verificar si las dos mitades del arreglo son iguales
-        for (int i = 0; i < N / 2; i++)
-        {
-            if (local_A[i] != local_A[i + N / 2])
-            {
-                iguales = 0;
-                break;
-            }
-        }
-
-        // Imprimir el resultado de la verificación
-        if (iguales)
+    // Imprimir el resultado de la verificación
+    if (rank == 0)
+    {
+        if (global_iguales)
         {
             printf("Son iguales\n");
         }
@@ -280,12 +275,9 @@ int main(int argc, char *argv[])
         }
         printf("\n");
     }
-    printf("Llego el rank: %d\n", rank);
+
     // Liberar la memoria
-    if (rank == 0)
-    {
-        free(A);
-    }
+    free(A);
     free(local_A);
     free(local_B);
 
